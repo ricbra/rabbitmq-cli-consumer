7905beb6c1ecaa593ab10443871d1a9ca4434cf7
Richard van den Brand <richard@vandenbrand.org>
2015-01-07 20:46:42 +0000

    Improved build script

--- build_deb.sh
+++ build_deb.sh
@@ -4,17 +4,14 @@ ARCHS['amd64']='amd64'
 ARCHS['386']='i386'
 
 DIR=`mktemp -d`
-echo $DIR
-
 
 for ARCH in 'amd64' '386'
 do
     BUILD=${ARCHS[$ARCH]}
     gox -osarch="linux/$ARCH" -output="$DIR/$ARCH/usr/bin/{{.Dir}}"
     fpm -s dir -t deb -v $1 -n rabbitmq-cli-consumer -a $BUILD $DIR/$ARCH
-#    cp -r $DIR/linux/$ARCH .
 done
 
-#rm -rf $DIR
+rm -rf $DIR
 
 
