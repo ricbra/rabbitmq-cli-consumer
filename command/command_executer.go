2065eb6f63f292e654ac46d93693b33708e3fb31
Richard van den Brand <richard@vandenbrand.org>
2015-01-08 20:53:16 +0000

    Fixed logging messages

--- command/command_executer.go
+++ command/command_executer.go
@@ -20,13 +20,15 @@ func New(errLogger, infLogger *log.Logger) *CommandExecuter {
 func (me CommandExecuter) Execute(cmd *exec.Cmd) bool {
 	me.infLogger.Println("Processing message...")
 	out, err := cmd.CombinedOutput()
-	me.infLogger.Println("Processed!")
 
 	if err != nil {
 		me.infLogger.Println("Failed. Check error log for details.")
 		me.errLogger.Printf("Failed: %s\n", string(out[:]))
+		me.errLogger.Printf("Error: %s\n", err)
 		return false
 	}
+
+	me.infLogger.Println("Processed!")
 
 	return true
 }
