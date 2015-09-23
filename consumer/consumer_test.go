11c4f0b1ebc06d40d7cb97e07522a3c1aa66a9be
Richard van den Brand <richard@vandenbrand.org>
2015-09-23 21:36:58 +0200

    Added TestBindQueueSucceeds

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -115,6 +115,25 @@ func TestBindQueueFails(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+func TestBindQueueSucceeds(t *testing.T) {
+	config := createConfig()
+	ch := new(TestChannel)
+
+	var b bytes.Buffer
+	errLogger := log.New(&b, "", 0)
+	infLogger := log.New(&b, "", 0)
+
+	ch.On("Qos", 3, 0, true).Return(nil).Once()
+	ch.On("QueueDeclare", "worker", true, false, false, false, amqp.Table{}).Return(amqp.Queue{}, nil).Once()
+	ch.On("ExchangeDeclare", "worker", "test", true, false, false, false, amqp.Table{}).Return(nil).Once()
+	ch.On("QueueBind", "worker", "", "worker", false, amqp.Table{}).Return(nil).Once()
+
+	err := Initialize(&config, ch, errLogger, infLogger)
+
+	ch.AssertExpectations(t)
+	assert.Nil(t, err)
+}
+
 type TestChannel struct {
 	mock.Mock
 }
